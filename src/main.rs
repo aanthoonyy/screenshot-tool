10d7ffbd0e98554771fbe803d27f93b68f80a888
anthony <53194069+aanthoonyy@users.noreply.github.com>
2025-07-04 10:56:51 -0700

    file string name

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+#![allow(unused)]
+use std::time::SystemTime;
+use chrono::{DateTime, Utc};
+
 fn main() {
-    println!("Hello world!");
+    
+    let sys_time = SystemTime::now();
+    println!("{:?}", sys_time);
+
+    let datetime: DateTime<Utc> = DateTime::<Utc>::from(sys_time);
+    let formatted = datetime.format("%Y-%m-%d-%H:%M:%S").to_string();
+    println!("screenshot-{}", formatted);
 }
